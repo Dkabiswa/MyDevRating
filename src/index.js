8f8b16e2a3caf0fcd523b811663bf9d4ca404523
Kabiswa Davis <kabiswad@gmail.com>
2019-12-08 21:52:46 +0200

    ch(heroku): add build script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import '@babel/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from '../src/components/App';
+import App from './components/App';
 import './styles/authLogin.scss';
-import './styles/table.scss'
+import './styles/table.scss';
 
 render(<Router><App /></Router>, document.getElementById('app'));
